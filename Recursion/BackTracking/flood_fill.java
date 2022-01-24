b81c72143b90222323eb022ef30814797e5cc8ab
saisameerd <saisameer.d@gmail.com>
2022-01-24 23:29:59 +0530

    flood_fill

---
+++ Recursion/BackTracking/flood_fill.java
@@ -0,0 +1,40 @@
+package Recursion.BackTracking;
+
+
+import java.util.Scanner;
+
+public class flood_fill {
+    public static void main(String[] args) throws Exception {
+         Scanner  br = new  Scanner (System.in);
+        int n = br.nextInt();
+        int m = br.nextInt();
+        int[][] arr = new int[n][m];
+  
+        for (int i = 0; i < n; i++) {
+           for (int j = 0; j < m; j++) {
+              arr[i][j] = br.nextInt();
+           }
+        }
+  
+        floodfill(arr, 0, 0, "", new boolean[n][m]);  
+
+        br.close();
+     }
+  
+     public static void floodfill(int[][] maze, int row, int col, String psf, boolean[][] visited){
+        if(row < 0 || col < 0 || row >= maze.length || col >= maze[0].length || 
+           maze[row][col] == 1 || visited[row][col] == true){
+           return;
+        } else if(row == maze.length - 1 && col == maze[0].length - 1){
+           System.out.println(psf);
+           return;
+        }
+  
+        visited[row][col] = true;
+        floodfill(maze, row - 1, col, psf + "t", visited);
+        floodfill(maze, row, col - 1, psf + "l", visited);
+        floodfill(maze, row + 1, col, psf + "d", visited);
+        floodfill(maze, row, col + 1, psf + "r", visited);
+        visited[row][col] = false;
+     }
+}
