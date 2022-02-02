e8bab8d62d90a141177295e365367a020a3bfd0b
saisameerd <saisameer.d@gmail.com>
2022-02-02 23:17:22 +0530

    get_valueAt

---
+++ LinkedList/get_valueAt.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+public class get_valueAt {
+    public static class Node {
+        int data;
+        Node next;
+      }
+    
+      public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        
+    public int getFirst(){
+        if(size == 0){
+          System.out.println("List is empty");
+          return -1;
+        } else {
+          return head.data;
+        }
+      }
+  
+      public int getLast(){
+        if(size == 0){
+          System.out.println("List is empty");
+          return -1;
+        } else {
+          return tail.data;
+        }
+      }
+  
+      public int getAt(int idx){
+        if(size == 0){
+          System.out.println("List is empty");
+          return -1;
+        } else if(idx < 0 || idx >= size){
+          System.out.println("Invalid arguments");
+          return -1;
+        } else {
+          Node temp = head;
+          for(int i = 0; i < idx; i++){
+            temp = temp.next;
+          }
+          return temp.data;
+        }
+      } 
+}
+}
--- LinkedList/remove_first.java
+++ LinkedList/remove_first.java
@@ -23,3 +23,4 @@ public class remove_first {
         }
       }
 }
+}
