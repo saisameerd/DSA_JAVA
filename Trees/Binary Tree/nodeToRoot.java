029cb03ddffe768be2e7003ee5511621f83ea8f1
saisameerd <saisameer.d@gmail.com>
2022-04-18 17:27:27 +0530

    node tor root and LC 236 done

---
+++ Trees/Binary Tree/nodeToRoot.java
@@ -0,0 +1,58 @@
+public class nodeToRoot {
+    /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+    
+    public ArrayList<TreeNode> nodeToRoot(TreeNode root , int data){
+        if(root == null){
+            return new ArrayList<>();
+        }
+        
+        if(root.val == data){
+            ArrayList<TreeNode> base = new ArrayList<>();
+            base.add(root);
+            return base;
+        }
+        
+        ArrayList<TreeNode> leftAns = nodeToRoot( root.left , data);
+        if(leftAns.size() > 0){
+            leftAns.add(root);
+            return leftAns;
+        }
+        
+        ArrayList<TreeNode> rightAns = nodeToRoot(root.right , data);
+        if(rightAns.size() > 0){
+            rightAns.add(root);
+            return rightAns;
+        }
+        
+        return new ArrayList<>();
+    }
+    
+    
+    // LEETCODE 236 MEDIUM
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        ArrayList<TreeNode> pathP = nodeToRoot(root , p.val);
+        ArrayList<TreeNode> pathQ = nodeToRoot(root, q.val);
+        
+        int i = pathP.size() - 1;
+        int j = pathQ.size() - 1;
+        TreeNode Lca = null;
+        while(i>=0 && j>=0){
+            if(pathP.get(i) != pathQ.get(j))
+                break;
+            Lca = pathP.get(i);
+            i--;
+            j--;
+        }
+        
+        return Lca;
+    }
+}
