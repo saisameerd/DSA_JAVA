07e943a30a02a94ff41997e6007d49a759e2cc17
saisameerd <saisameer.d@gmail.com>
2022-05-03 15:02:21 +0530

    hamiltonPathorCycle done

---
+++ Graphs/hamiltonPathorCycle.java
@@ -0,0 +1,35 @@
+public class hamiltonPathorCycle {
+    public void pathOrCycle(ArrayList<Edge>graph , boolean[] vis , int src , int osrc , String asf){
+
+        if(asf.length() == graph.length){
+            System.out.println(asf);
+
+            for(Edge e : graph[osrc]){
+                int nbr = e.nbr ;
+
+                if(nbr == src){
+                    System.out.println("cycle");
+                    return ;
+
+                }
+
+
+            }
+
+            System.out.println("path");
+            return ;
+        }
+
+
+
+        vis[src] = true ;
+
+        for(Edge e: graph[src]){
+            int nbr = e.nbr;
+
+            pathOrCycle(graph, vis, nbr, osrc, asf + nbr);
+        }
+
+        vis[src] = false ;
+    }
+}
